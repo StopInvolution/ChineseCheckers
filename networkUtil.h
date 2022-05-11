57ae35a68fdad84d6d819f0e40e0795397f26f4a
panjd123 <1747366367@qq.com>
2022-05-12 00:55:27 +0800

    完善了 ChessBoard 类对服务器信号的处理，添加了若干数据解析函数
    loadPlayerName

--- networkUtil.h
+++ networkUtil.h
@@ -3,6 +3,8 @@
 #include "settings.h"
 #include <vector>
 #include <QString>
+void loadPlayerName(std::vector<QString>& vec,QString data);
+void loadReady(std::vector<bool>& vec,QString data);
 void loadChessPosition(std::vector<ChessPosition>& vec,QString data);
 std::vector<ChessPosition> loadChessPosition(QString data);
 #endif // NETWORKUTIL_H
